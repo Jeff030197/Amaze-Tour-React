29c085dbe1360566420f87b2512a3ff56afab9d3
unknown <edwardparias3@gmail.com>
2023-07-04 01:42:50 +0800

    website

---
+++ src/Components/Content.js
@@ -0,0 +1,31 @@
+import {Carousel, Container,} from 'react-bootstrap';
+import Car1 from "./../img/1.jpg";
+import Car2 from "./../img/2.jpg";
+import Car3 from "./../img/3.jpg";
+const Content = () => {
+    return ( 
+        <Carousel className="yes">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Car1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Car3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+      
+
+
+
+
+     );
+}
+ 
+export default Content;
---
+++ src/Components/loading.js
@@ -0,0 +1,47 @@
+import { Button, Offcanvas, Card, Row, Col, } from 'react-bootstrap';
+import React from 'react';
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { useState } from 'react';
+import like from "./../img/like.jpg";
+const Loading = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+
+    <MDBRow>
+        <Row>
+          <Col/>
+          <Col className='d-flex justify-content-center' ><Button variant="primary" onClick={handleShow} className="papel">
+          Promo of the day
+        </Button></Col>
+        <Col/> 
+        </Row>
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Like Promo</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={like} />
+              <Card.Body>
+                <Card.Title>Like us and get discount</Card.Title>
+                <Card.Text>
+                  Lorem Lorem sinta buko ng papaya
+                </Card.Text>
+                <Button variant="primary" href="https://www.facebook.com/amazingescapesph">Like us here</Button>
+              </Card.Body>
+            </Card>
+          </Offcanvas.Body>
+        </Offcanvas>
+    </MDBRow>
+  );
+}
+
+export default Loading;
+
+
+
+
+
